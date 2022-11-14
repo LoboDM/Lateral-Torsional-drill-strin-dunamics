734c9b536696b2e642564e6434c38208ec73b951
LoboDM <daniel.lobo23@gmail.com>
2022-11-14 10:40:24 -0300

    Returned to nearest method

--- Equations/Hs_extract.m
+++ Equations/Hs_extract.m
@@ -1,22 +1,24 @@
 function H_s = Hs_extract(H_grid,z_grid,theta_grid,theta,z)
     
-    theta_aux = rem(rem(theta,2*pi) + 2*pi,2*pi);
-    
-    theta_grid(:,end+1) = theta_grid(:,end) + theta_grid(:,2);
-    z_grid(:,end+1) = z_grid(:,end);
-    H_grid(:,end+1) = H_grid(:,1);
-    
-    H_s = interp2(theta_grid,z_grid,H_grid,theta_aux,z,'cubic',1e10);
-    
-%             H_map = H_grid(:);
-%         z_map = z_grid(:);
-%         theta_map = theta_grid(:);
-% 
-%         theta = rem(rem(y(i+2),2*pi) + 2*pi,2*pi);
-% 
-%         aux0 = find(theta_map<=theta, 1, 'last' );
-% 
-%         aux1 = find(z_map<=y(i));
-%         H_s = H_map(max(aux1(aux1<=aux0)));
+%     theta_aux = rem(rem(theta,2*pi) + 2*pi,2*pi);
+%     
+%     theta_grid(:,end+1) = theta_grid(:,end) + theta_grid(:,2);
+%     z_grid(:,end+1) = z_grid(:,end);
+%     H_grid(:,end+1) = H_grid(:,1);
+%     
+%     H_s = interp2(theta_grid,z_grid,H_grid,theta_aux,z,'nearest',1e10);
+
+H_map = H_grid(:);
+z_map = z_grid(:);
+theta_map = theta_grid(:);
+
+H_s = zeros(size(theta));
+for ii = 1:length(theta)
+    theta_aux = rem(rem(theta(ii),2*pi) + 2*pi,2*pi);
+
+    aux0 = find(theta_map<=theta_aux, 1, 'last' );
+
+    aux1 = find(z_map<=z(ii));
+    H_s(ii) = H_map(max(aux1(aux1<=aux0)));
     
 end
